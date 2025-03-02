e4e1dec5b20039f8a600a3e6c79aa932a336cb9c
Jan Kneissler <jan.kneissler@breuninger.de>
2025-03-02 13:17:37 +0100

    moved to top folder

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
 # Sets up an orchestrator instance used for testing
 
 module "infrastructure" {
-  source = "./infrastructure"
+  source = "infrastructure"
   project_id = "green-k8s"
   dataset_location = "eu"
 }
