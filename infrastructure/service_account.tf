e2392d1bbbcf0d521a809120926b2e43a38b5531
Jan Kneissler <jan.kneissler@breuninger.de>
2025-03-02 17:04:42 +0100

    iam member compute

--- infrastructure/service_account.tf
+++ infrastructure/service_account.tf
@@ -3,6 +3,16 @@ resource "google_service_account" "cloud_function_service_account" {
   account_id   = "frontend-service-account"
   display_name = "Service account for frontend"
   description  = "This service account is used to run the cloud function that serves request in the front end"
-  project      = "green-k8s"
+  project      = var.project_id
 }
 
+// needed to get project number
+data "google_project" "project" {
+  project_id = var.project_id
+}
+
+resource "google_service_account_iam_member" "member" {
+  service_account_id = "projects/-/serviceAccounts/${data.google_project.project.number}-compute@developer.gserviceaccount.com"
+  role               = "roles/iam.serviceAccountUser"
+  member             = "serviceaccount:${google_service_account.cloud_function_service_account.email}"
+}
